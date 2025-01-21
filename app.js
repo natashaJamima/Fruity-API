6b8e5cfc479a178545d52162f85cb5d971731c02
natashaJamima <121585035+natashaJamima@users.noreply.github.com>
2025-01-21 09:57:01 +0000

    Add cors to project

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const fruits = require("./routes/fruits");
 const logger = require("./loggers");
 // Middleware
+
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 
