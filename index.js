3a4e290082e4ed4c5472760636c25d15304a31a4
natashaJamima <121585035+natashaJamima@users.noreply.github.com>
2025-01-21 09:38:16 +0000

    Add .env to  project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const app = require("./app")
-const port = 3000;
+require("dotenv").config();
+
+const app = require("./app");
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
